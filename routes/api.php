6efb01ce5396be3c66db0efbc5e0f5d39ffa755a
hodaa <hoda@hussin@gmail.com>
2021-10-31 14:52:57 +0200

    remove line

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
-
-
 Route::post('/me', [\App\Http\Controllers\AuthController::class, 'me'])->middleware(['auth:sanctum']);
